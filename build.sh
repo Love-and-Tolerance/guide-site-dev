cfae91a17a537dff390ed6ff80ce12c3f1b4e172
Silk Rose <silkrose@love-tolerance.com>
2022-12-30 21:13:21 -0500

    make build.sh posix compliant, add mdbook install

--- build.sh
+++ build.sh
@@ -1,9 +1,42 @@
-#!/usr/bin/env bash
+#!/usr/bin/env sh
 
-declare -a folders=("css" "FontAwesome" "fonts")
-declare -a files=("404.html" "ayu-highlights.css" "book.js" "clipboard.min.js" "elsasticlunr.min.js" "highlight.css" "highlight.js" "mark.min.js" "print.html" "searcher.js" "searchindex.js" "searchindex.json" "tomorrow-night.css" ".nojekyll")
+if test -z "$(command -v mdbook 2> /dev/null)"; then
+    printf "mdbook is required to build, do you want to install it? (y/n) "
+    read -r response
+    case $response in
+      [Yy]*)
+        curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+        cargo install mdbook
+        ;;
+      [Nn]*)
+        exit
+        ;;
+      *)
+        echo "Invalid response"
+        exit 1
+        ;;
+    esac
+fi
 
-for i in "${folders[@]}"
+folders="css, FontAwesome, fonts"
+
+files="404.html, ayu-highlights.css, book.js, clipboard.min.js, elsasticlunr.min.js, highlight.css, highlight.js, ,mark.min.js, print.html, searcher.js, searchindex.js, searchindex.json, tomorrow-night.css, .nojekyll"
+
+if [ -d "./deploy" ]; then
+    rm -rf ./deploy
+fi
+
+mkdir ./deploy
+
+for folder in $folders
 do
-   echo "$i"
+   echo "$folder" | tr -d ' ,'
 done
+
+for dir in ./src/guides/*/
+do
+    dir=${dir%*/}
+    echo "$dir"
+    echo "${dir##*/}"
+done
+
