5bd7bb28619a5be141996cda6b1434de2225c9ac
Silk Rose <silkrose@love-tolerance.com>
2022-12-31 21:07:19 -0500

    finish build script

--- build.sh
+++ build.sh
@@ -18,25 +18,49 @@ if test -z "$(command -v mdbook 2> /dev/null)"; then
     esac
 fi
 
-folders="css, FontAwesome, fonts"
-
-files="404.html, ayu-highlights.css, book.js, clipboard.min.js, elsasticlunr.min.js, highlight.css, highlight.js, ,mark.min.js, print.html, searcher.js, searchindex.js, searchindex.json, tomorrow-night.css, .nojekyll"
-
 if [ -d "./deploy" ]; then
     rm -rf ./deploy
 fi
 
 mkdir ./deploy
 
-for folder in $folders
-do
-   echo "$folder" | tr -d ' ,'
-done
+cd src/site || exit 2
+rm -rf guide theme
+mkdir guide theme
+cp -r ../theme/* ./theme/
+mv ./theme/site-index.hbs ./theme/index.hbs
+rm ./theme/guide-index.hbs
+mdbook build
+cd ../..
+cp -r src/site/guide/* ./deploy
+cp src/site/guide/.nojekyll ./deploy
 
-for dir in ./src/guides/*/
+folders="css, FontAwesome, fonts, theme"
+
+files="404.html, ayu-highlight.css, book.js, clipboard.min.js, elasticlunr.min.js, favicon.png, favicon.svg, highlight.css, highlight.js, ,mark.min.js, print.html, searcher.js, searchindex.js, searchindex.json, tomorrow-night.css, .nojekyll"
+
+for guide in ./src/guides/*/
 do
-    dir=${dir%*/}
-    echo "$dir"
-    echo "${dir##*/}"
+    guide_path=${guide%*/}
+    guide_name=${guide_path##*/}
+    echo "$guide_name"
+    echo "$guide_path"
+    cd "$guide_path" || exit 2
+    rm -rf guide theme
+    mkdir guide theme
+    cp -r ../../theme/* ./theme/
+    mv ./theme/guide-index.hbs ./theme/index.hbs
+    rm ./theme/site-index.hbs
+    mdbook build
+    cd guide || exit 2
+    for folder in $folders; do
+        rm -rf "$(echo "$folder" | tr -d ",")"
+    done
+    for file in $files; do
+        rm -rf "$(echo "$file" | tr -d ",")"
+    done
+    cd ../../../..
+    mkdir deploy/"$guide_name"
+    cp -r "$guide_path"/guide/* ./deploy/"$guide_name"/
 done
 
